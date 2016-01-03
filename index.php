da8ded68f3169130775ba1816f64aabb608a33d1
dbk3r <bk.pc@gmx.de>
2016-01-03 14:40:34 +0100

    add jquery migrate 1.2.1

--- index.php
+++ index.php
@@ -2,3 +2,26 @@
 
 	include_once 'includes/config.php';
 	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title>CC v-0.1</title>
+    	
+    	<link rel="stylesheet" href="./includes/css/cc.css" rel="stylesheet" />
+    	
+	</head>
+	<body>
+		<script type="text/javascript" src="./includes/js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="./includes/js/jquery-migrate-1.2.1.min.js"></script>
+		<script type="text/javascript" src="./includes/js/dmuploader.js"></script>
+		
+		<script></script>
+		
+	</body>
+	
+</html>
