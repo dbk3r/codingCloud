9d2a80d2bb6e55e5437d74e3dd99b8abe30818e1
dbk3r <bk.pc@gmx.de>
2016-01-08 10:50:01 +0100

    file-uploader change css styles

--- index.php
+++ index.php
@@ -31,24 +31,19 @@
 				<table width=100%>
 					<tr>
 						<td align=center>
-						<form id="upload" method="post" action="upload.php" enctype="multipart/form-data">
-							<div id="drop">
-								Drop Here
-				
-								<a>Browse</a>
-								<input type="file" name="upl" multiple />
-							</div>
-				
-							<ul>
-								<!-- The file uploads will be shown here -->
-							</ul>
-				
-						</form>
-					      
-						</td>
-						
-						<td align=center><div class="upload_db" id="upload-blender">Blender</div></td>
+							<form id="upload" method="post" action="upload.php" enctype="multipart/form-data">
+								<div id="drop">
+									Datei hier Fallen lassen<br>
+					
+									<a>Auswahl</a>
+									<input type="file" name="upl" multiple />
+								</div>
+					
+								<ul></ul>				
+							</form>					      
+						</td>					
 					</tr>
+					
 				</table>
 			</div>
 			
