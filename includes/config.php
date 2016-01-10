4b213f3497e2eaeca996aa842fa4046b6cbe647b
dbk3r <bk.pc@gmx.de>
2016-01-10 14:40:26 +0100

    DB table fixes

--- includes/config.php
+++ includes/config.php
@@ -3,14 +3,14 @@
  * Das sind die Login-Angaben für die Datenbank
  */  
 define("HOST", "localhost");     // Der Host mit dem du dich verbinden willst.
-define("BRC_ADMIN", "cc_admin");    // Der Datenbank-Benutzername. 
-define("BRC_ADMIN_PASSWORD", "ccpasswd");    // Das Datenbank-Passwort. 
+define("CC_ADMIN", "cc_admin");    // Der Datenbank-Benutzername. 
+define("CC_ADMIN_PASSWORD", "ccpasswd");    // Das Datenbank-Passwort. 
 define("DB", "cc_db");    // Der Datenbankname.
 define("TABLE_PREPIX", "cc_");    // Der Datenbankname.
 
 define("CONTENT_DIR", "/var/www/html/cc/content/");    // Content/Data Verzeichnis.
 define("UPLOAD_DIR", "/var/www/html/cc/file-upload/");    // Upload Verzeichnis.
-define("PUBLISH_DIR", "/mnt/dps-p-austausch/CodingCloud/");    // mountpoint Samba Verzeichnis.
+define("PUBLISH_DIR", "/mnt/nas/cc_share/CodingCloud/");    // mountpoint Samba Verzeichnis.
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
