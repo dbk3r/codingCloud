0ad476fd5eb19b3ffcacb80c83ac2015b45689ff
dbk3r <bk.pc@gmx.de>
2016-01-10 19:34:13 +0100

    code cleanup

--- includes/content_allowed.php
+++ includes/content_allowed.php
@@ -1,7 +1,7 @@
 <?php
 $contentType_Audio = array('m4a', 'mp3','wav', 'wma', 'flac');
 $contentType_Video = array('avi','mov', 'mxf', 'mp4', 'mkv', '3gp', 'm4v');
-$contentType_Xtra = array('zip');
+$contentType_Xtra = array('zip', 'blend');
 
 $allowed = array_merge($contentType_Audio, $contentType_Video, $contentType_Xtra);
 
