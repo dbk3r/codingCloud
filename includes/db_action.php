b55f1942e9b9808426f936427d06682794eed9ad
dbk3r <bk.pc@gmx.de>
2016-01-11 21:19:20 +0100

    add del action

---
+++ includes/db_action.php
@@ -0,0 +1,17 @@
+<?php
+
+	include_once 'config.php';
+	include_once 'functions.php';
+	include_once 'db_connect.php';
+
+	if(isset($_POST['del'])) {
+		$db = DB;
+		$id = $_POST['del'];
+ 		$sql = "delete from ".$db.".cc_content where content_uuid='$id'";
+		$mysqli->query($sql) ;
+		
+	}
+
+
+	include_once 'db_disconnect.php';
+?>
