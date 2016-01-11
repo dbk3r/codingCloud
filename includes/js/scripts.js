cad19316b3e4b2d1e2598bc73ddd031554e8454c
dbk3r <bk.pc@gmx.de>
2016-01-11 22:16:15 +0100

    delete content directly from list

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -1,13 +1,6 @@
 
-$(document).ready( function() {
- 				
- 	$(".content").mouseover(function() {
-		console.log("id: " + this.id);
-	});
-		
-	$(".content").click(function() {
-		alert("id: " + this.id);
-	});
+$(document).ready( function() { 				
+ 	
 		
 	$(window).resize();
 	
@@ -15,16 +8,48 @@ $(document).ready( function() {
 	
 		$("#tbl-content").load('includes/content.php',function() {
 			$(".content").mouseover(function() {
-				console.log("id: " + this.id);
+				
 			});
 		
 			$(".content").click(function() {
-				alert("id: " + this.id);
-			});
 				
-		});		
+			});
+			
+			$("#"+this.id+" .trash").mouseover(function() {
+				$("#"+this.id+" .trash").css({
+					border:'1px solid #000'
+				});
+			});
+			$("#"+this.id+" .trash").mouseout(function() {
+				$("#"+this.id+" .trash").css({
+					border:'0px solid #000'
+				});
+			});
+			
+			$("#"+this.id+" .trash").mousedown(function() {
+				$("#"+this.id+" .trash").css({
+					border:'1px solid #ff0000'
+				});
+			});
+			
+			
+			$(".trash").dblclick(function() {
+							
+				$("#"+this.id).remove(),
+				$.ajax({
+				    url: "includes/db_action.php",
+				    type: "POST",
+				    data: 'del='+this.id,
+				    success: function(data) {
+				            
+				    },  
+				});
+				
+			}); // end trash click
+				
+		}); // end content reload
     
-	}, 2500);		
+	}, 2000);		
  				
 });
 
