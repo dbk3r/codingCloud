d5e2b690da0b110389b6790a439042df621e908a
dbk3r <bk.pc@gmx.de>
2016-01-12 18:40:19 +0100

    add settings

--- includes/search-bar.php
+++ includes/search-bar.php
@@ -1,12 +1,18 @@
 <div id=search-bar class=content-head>
+	
+	<div id=search-btn class="search">
+		<img  src=/img/search.png class="search-btn">
+	</div>
+	
 	<div id=search-filter class="search">
-		Audio <input type=checkbox id=audio-filter name=audio-filter>
-		Video <input type=checkbox id=video-filter name=video-filter>		
-		Blender <input type=checkbox id=blender-filter name=blender-filter>
+		<input type=text id=search-input name=search-input class="search-input">
 	</div>
 	<div id=search-filter class="search">
-		<input type=text id=search-input name=search-input class="search-input"><img  src=/img/search.png>
+		<input type=checkbox id=audio-filter name=audio-filter class=search-cb> <span class=search-filter>Audio</span>
+		<input type=checkbox id=video-filter name=video-filter class=search-cb> <span class=search-filter>Video</span>	
+		<input type=checkbox id=blender-filter name=blender-filter class=search-cb> <span class=search-filter>Blender</span>
 	</div>
+	
 <?php
 
 
