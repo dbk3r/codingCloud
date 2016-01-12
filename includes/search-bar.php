7215c53b742104a8dca583c903ca2934dab7e855
dbk3r <bk.pc@gmx.de>
2016-01-12 12:50:38 +0100

    add search bar

---
+++ includes/search-bar.php
@@ -0,0 +1,14 @@
+<div id=search-bar class=content-head>
+	<div id=search-filter class="search">
+		Audio <input type=checkbox id=audio-filter name=audio-filter>
+		Video <input type=checkbox id=video-filter name=video-filter>		
+		Blender <input type=checkbox id=blender-filter name=blender-filter>
+	</div>
+	<div id=search-filter class="search">
+		<input type=text id=search-input name=search-input class="search-input"><img  src=/img/search.png>
+	</div>
+<?php
+
+
+?>
+</div>
