f3df5f575a593b50852eaa7be317cc2016e677de
dbk3r <bk.pc@gmx.de>
2016-01-12 21:37:42 +0100

    add admin and user menue

--- index.php
+++ index.php
@@ -66,10 +66,10 @@
 			
 			<div id=settings class=settings>
 				
-				<a id=data-profile href="#" data-jq-dropdown="#jq-menu-profile" data-horizontal-offset="-6" class="settings-profile p-margin">
+				<a title="Benutzer Einstellungen" id=data-profile href="#" data-jq-dropdown="#jq-menu-profile" data-horizontal-offset="-6" class="settings-profile p-margin">
 						<img src="img/arrow.png" class="settings-profile p-margin"><img id=a-avatar src="img/avatar.png" class="settings-profile">
 				</a>		
-				<a id=data-admin href="#" data-jq-dropdown="#jq-menu-admin" data-horizontal-offset="-6" class="settings-profile p-margin">
+				<a title="Admin Einstellungen" id=data-admin href="#" data-jq-dropdown="#jq-menu-admin" data-horizontal-offset="-6" class="settings-profile p-margin">
 					<img src="img/arrow.png" class="settings-profile p-margin"><img id=a-admin src="img/tool.png" class="settings-profile">
 				</a>
 				
